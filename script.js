fe8fe784eda5464e7413a877a6b9bc1feb0fbaa7
mini-chan <mlintanffcas@etudiant.univ-rennes1.fr>
2024-05-19 01:27:35 +0200

    avancé sur rebriques

---
+++ script.js
@@ -0,0 +1,52 @@
+ // Fonction pour définir un cookie avec une durée d'expiration
+ function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Fonction pour obtenir la valeur d'un cookie
+function getCookie(name) {
+    const cookieName = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    let cookie = cookieArray[0];
+        
+    return cookie.substring(cookieName.length, cookie.length);
+}
+
+
+function toggleDarkMode() {
+    const body = document.body;
+    const modeToggle = document.getElementById('modeToggle');
+    const themeLabel = document.getElementById('themeLabel');
+
+    if (modeToggle.checked) {
+        body.classList.add('dark-mode');
+        themeLabel.textContent = 'Thème sombre';
+        setCookie('theme', 'dark', 30); // Définir le cookie avec une durée d'expiration de 30 jours
+    } else {
+        body.classList.remove('dark-mode');
+        themeLabel.textContent = 'Thème clair';
+        setCookie('theme', 'light', 30); // Définir le cookie avec une durée d'expiration de 30 jours
+    }
+}
+
+// Vérifier le cookie au chargement de la page et appliquer le thème en conséquence
+window.onload = function() {
+    const savedTheme = getCookie('theme');
+    const modeToggle = document.getElementById('modeToggle');
+    const themeLabel = document.getElementById('themeLabel');
+    const body = document.body;
+
+    if (savedTheme === 'dark') {
+        body.classList.add('dark-mode');
+        modeToggle.checked = true;
+        themeLabel.textContent = 'Thème sombre';
+    } else {
+        body.classList.remove('dark-mode');
+        modeToggle.checked = false;
+        themeLabel.textContent = 'Thème clair';
+    }
+};
